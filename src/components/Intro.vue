9389673bde46fa254022307d9227ba66ef6bfbe5
mariehmai <mai.mariehelene@gmail.com>
2020-07-13 09:46:37 +0200

    feat(Intro): added me description and contact scroll to footer

---
+++ src/components/Intro.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="intro">
+    <v-container>
+      <h1 class="mb-10">Hi, I'm Marie-Hélène, Full Stack Developer ✌️</h1>
+      <v-btn outlined color="inverted" @click="scrollToBottom"
+        >Contact me</v-btn
+      >
+    </v-container>
+  </div>
+</template>
+
+<script>
+export default {
+  computed: {
+    pageHeight() {
+      return document.body.scrollHeight
+    }
+  },
+  methods: {
+    scrollToBottom() {
+      this.$vuetify.goTo(this.pageHeight)
+    }
+  }
+}
+</script>
+
+<style scoped>
+.intro {
+  height: 75vh;
+  background: linear-gradient(90deg, #ff5f6d 0%, #ffc371 100%);
+  color: whitesmoke;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
