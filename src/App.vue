9598aa9f0c0cb765b5fe4826321539c16c60b505
mariehmai <mai.mariehelene@gmail.com>
2020-07-13 09:48:45 +0200

    feat(App): added display of intro work and intro into app page

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,27 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+    <v-main app>
+      <Intro />
+      <Work />
+    </v-main>
+
+    <v-footer>
+      <Footer />
+    </v-footer>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Footer from '@/components/Footer'
+import Intro from '@/components/Intro'
+import Work from '@/components/Work'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Footer,
+    Intro,
+    Work
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
