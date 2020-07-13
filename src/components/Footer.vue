aa099a9d4f0155d8d1adf268ae928dbcaa4bf78a
mariehmai <mai.mariehelene@gmail.com>
2020-07-13 09:45:57 +0200

    feat(Footer): added social links in footer

---
+++ src/components/Footer.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-container>
+    <span class="text-caption mr-10">MH © 2020</span>
+    <a href="https://github.com/mariehmai" target="_blank">
+      <v-btn icon>
+        <font-awesome-icon :icon="['fab', 'github']" />
+      </v-btn>
+    </a>
+    <a href="https://linkedin.com/in/mariehmai" target="_blank">
+      <v-btn icon>
+        <font-awesome-icon :icon="['fab', 'linkedin']" />
+      </v-btn>
+    </a>
+    <a href="mailto:mai.mariehelene@gmail.com">
+      <v-btn icon>
+        <font-awesome-icon :icon="['fas', 'envelope']" />
+      </v-btn>
+    </a>
+  </v-container>
+</template>
+
+<script>
+export default {}
+</script>
