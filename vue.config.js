d7d4fbc6965c1aff01275884ba035179098f920f
mariehmai <mai.mariehelene@gmail.com>
2020-07-14 07:18:55 +0200

    feat(favicon): updated favicon

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@ module.exports = {
   transpileDependencies: ['vuetify'],
   chainWebpack: (config) => {
     config.plugin('html').tap((args) => {
-      args[0].title = 'Marie-h Mai'
+      args[0].title = 'Marie-h'
       return args
     })
   }
